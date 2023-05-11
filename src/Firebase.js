f04839dabeaa6c5b48098258871520159d0c4883
Hamza Arshad <hamza.arshad@visionx.io>
2023-05-11 00:08:22 -0700

    firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_ZHOMTqH_JHq_r3bQy3AgBW1YrOWPKjY",
-  authDomain: "airbnb-react-auth.firebaseapp.com",
-  projectId: "airbnb-react-auth",
-  storageBucket: "airbnb-react-auth.appspot.com",
-  messagingSenderId: "1078496445290",
-  appId: "1:1078496445290:web:43ae7d4d0feeb405f11b13",
-  measurementId: "G-VS97E9GKYG",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTID,
 };
 
 // Initialize Firebase
